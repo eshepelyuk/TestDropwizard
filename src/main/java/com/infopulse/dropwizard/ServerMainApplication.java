a88fba7caaa2d5e5e52679f6c992e6d0a8cce829
Ievgenii.Shepeliuk <Ievgenii.Shepeliuk@infopulse.com>
2016-02-04 09:56:40 +0200

    CORS

--- src/main/java/com/infopulse/dropwizard/ServerMainApplication.java
+++ src/main/java/com/infopulse/dropwizard/ServerMainApplication.java
@@ -9,7 +9,14 @@ import io.dropwizard.jdbi.DBIFactory;
 import io.dropwizard.migrations.MigrationsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.skife.jdbi.v2.DBI;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
+
+import static org.eclipse.jetty.servlets.CrossOriginFilter.*;
 
 public class ServerMainApplication extends Application<ServerMainConfiguration> {
     public static void main(String[] args) throws Exception {
@@ -34,5 +41,14 @@ public class ServerMainApplication extends Application<ServerMainConfiguration> 
         final NewsItemDAO newsItemDAO = jdbi.onDemand(NewsItemDAO.class);
 
         environment.jersey().register(new NewsResource(newsItemDAO));
+
+        FilterRegistration.Dynamic filter = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+        filter.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), false, environment.getApplicationContext().getContextPath() + "*");
+
+        filter.setInitParameter(ALLOWED_ORIGINS_PARAM, "*");
+        filter.setInitParameter(ALLOWED_HEADERS_PARAM, "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin");
+        filter.setInitParameter(ALLOWED_METHODS_PARAM, "GET,PUT,POST,DELETE,OPTIONS");
+
+        filter.setInitParameter(ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*");
     }
 }
